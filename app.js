cdb0080242b8ea37ec30f3ceb4988dbfa5fb1428
martinetmayank <thedeveloper.mayank@gmail.com>
2020-08-07 19:00:47 +0530

    Installed packages

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const validator = require('validator')
+const chalk = require('chalk')
+
 const getNotes = require('./notes')
 msg = getNotes()
 
 console.log(msg)
+console.log(chalk.green.inverse.bold("Sucess"))
