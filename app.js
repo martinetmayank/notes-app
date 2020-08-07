d879a414a7b970686263628c06bb1ea05962bc01
martinetmayank <thedeveloper.mayank@gmail.com>
2020-08-07 21:24:09 +0530

    Arguments parsing

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
-const validator = require('validator')
 const chalk = require('chalk')
-
+const yargs = require('yargs')
 const getNotes = require('./notes')
-msg = getNotes()
+const {
+    demandOption
+} = require('yargs')
 
-console.log(msg)
-console.log(chalk.green.inverse.bold("Sucess"))
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ', argv.title)
+        console.log('Body: ', argv.body)
+    }
+})
+
+// Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing note...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('Reading note...')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all note',
+    handler: function () {
+        console.log('Listing all note...')
+    }
+})
+
+yargs.parse()
